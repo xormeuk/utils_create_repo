3b8d430475f5f718da06a362a44b22d0243018af
Matt Brocklehurst <m@xor.me.uk>
2024-03-22 21:03:09 +0000

    Working version, creates the repo on github and a local empty folder pointing at the new repo

--- create_repo.go
+++ create_repo.go
@@ -1,25 +1,67 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
-func executeCommand(name string, arg ...string) error {
-	cmd := exec.Command(name, arg...)
+// RepoRequest represents the JSON payload for creating a GitHub repository.
+type RepoRequest struct {
+	Name    string `json:"name"`
+	Private bool   `json:"private"`
+}
+
+func createGitHubRepo(token, username, repoName string) error {
+	apiUrl := "https://api.github.com/user/repos"
+	repo := RepoRequest{Name: repoName, Private: true}
+	repoJson, err := json.Marshal(repo)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(repoJson))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "token "+token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("failed to create repository: %s", string(body))
+	}
+
+	return nil
+}
+
+func executeGitCommand(args ...string) (string, error) {
+	cmd := exec.Command("git", args...)
 	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	output, err := cmd.Output()
+	return string(output), err
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run create_repo.go <repoName>")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: create_repo <GitHubUsername> <repoName>")
 		os.Exit(1)
 	}
-	repoName := os.Args[1]
-	username := os.Getenv("USER")
-	token := os.Getenv("GITHUB_TOKEN") // Ensure you have GITHUB_TOKEN set in your environment
+	username := os.Args[1]
+	repoName := os.Args[2]
+	token := os.Getenv("GITHUB_TOKEN")
 
 	if token == "" {
 		fmt.Println("GitHub token not found in environment variable GITHUB_TOKEN")
@@ -27,13 +69,12 @@ func main() {
 	}
 
 	// Create private repo on GitHub
-	if err := executeCommand("curl", "-X", "POST", "-H", "Authorization: token "+token, "-d", `{"name":"`+repoName+`","private":true}`, "https://api.github.com/user/repos"); err != nil {
-		fmt.Println("Failed to create repository on GitHub:", err)
+	if err := createGitHubRepo(token, username, repoName); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// Create local directory and initialize git
-	repoPath := "/home/" + username + "/dev/" + repoName
+	repoPath := "./" + repoName
 	if err := os.MkdirAll(repoPath, 0755); err != nil {
 		fmt.Println("Failed to create local directory:", err)
 		os.Exit(1)
@@ -44,14 +85,33 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := executeCommand("git", "init"); err != nil {
+	if _, err := executeGitCommand("init"); err != nil {
 		fmt.Println("Failed to initialize git:", err)
 		os.Exit(1)
 	}
-	if err := executeCommand("git", "remote", "add", "origin", "https://github.com/"+username+"/"+repoName+".git"); err != nil {
+	if _, err := executeGitCommand("remote", "add", "origin", "git@github.com:"+username+"/"+repoName+".git"); err != nil {
 		fmt.Println("Failed to add remote origin:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Repository", repoName, "created locally and on GitHub")
+	// Creating an initial commit
+	if _, err := executeGitCommand("commit", "--allow-empty", "-m", "Initial commit"); err != nil {
+		fmt.Println("Note: Failed to create initial commit. This can be normal if there are no files.")
+	}
+
+	// Dynamically identifying the default branch name
+	defaultBranch, err := executeGitCommand("branch", "--show-current")
+	if err != nil {
+		fmt.Println("Failed to identify the default branch name:", err)
+		os.Exit(1)
+	}
+	defaultBranch = strings.TrimSpace(defaultBranch)
+
+	// Setting upstream branch
+	if _, err := executeGitCommand("push", "--set-upstream", "origin", defaultBranch); err != nil {
+		fmt.Println("Failed to set upstream branch:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Repository", repoName, "created locally and on GitHub using SSH URL, with upstream set")
 }
